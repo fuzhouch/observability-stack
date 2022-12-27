5531b3ce0c2de1d89d4ae4c7c14ddf09a3fc643d
fuzhou.chen <fuzhouch@gmail.com>
2022-12-27 11:48:08 +0800

    [NEW] Simplify Loki deployment.
    Remove use of Grafana Agent and Prometheus Operator in monitoring and
    selfMonitoring section. We may use it in future.

--- loki/loki-onetime-setup.sh
+++ loki/loki-onetime-setup.sh
@@ -2,7 +2,7 @@
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update grafana
-helm install --upgrade \
+helm upgrade --install \
   --namespace loki \
   --values loki-helm-values.yaml \
   loki grafana/loki

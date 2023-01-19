6a6d5493f45ec08a885f2f9129646d096f129626
fuzhou.chen <fuzhouch@gmail.com>
2023-01-20 02:11:14 +0800

    [NEW] Add basic tempo cluster settings.

---
+++ tempo/tempo-onetime-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Based on documentation at
+# https://github.com/grafana/helm-charts/tree/main/charts/tempo-distributed
+
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update grafana
+helm upgrade --install \
+  --namespace loki \
+  --values tempo-helm-values.yaml \
+  tempo grafana/tempo-distributed

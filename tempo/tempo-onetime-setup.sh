0ca76e50c251ccb556b9b65328c02d64f2921fff
fuzhou.chen <fuzhouch@gmail.com>
2023-01-20 11:01:40 +0800

    [NEW] Add Tempo deployment.

--- tempo/tempo-onetime-setup.sh
+++ tempo/tempo-onetime-setup.sh
@@ -3,6 +3,7 @@
 # Based on documentation at
 # https://github.com/grafana/helm-charts/tree/main/charts/tempo-distributed
 
+kubectl apply -f tempo-namespace.yaml
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update grafana
 helm upgrade --install \
